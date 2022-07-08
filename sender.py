72d5c720d1f47cf9bb899719ec7ea514744a015f
tejeshsai <48875590+tejeshsai@users.noreply.github.com>
2022-07-08 16:09:42 +0530

    Modified Output format

--- sender.py
+++ sender.py
@@ -21,17 +21,27 @@ def send_params(sensor1, sensor2, receiver, no_of_samples):
     """
     Printing the values on console
     """
+    charge_stream = []
+    heat_stream = []
     for i in range(no_of_samples):  # creating 50 readings:
         attribute1 = random.randint(60, 80)
         attribute2 = random.randint(0, 60)
         charge_sensor = sensor1(attribute1)
         heat_sensor = sensor2(attribute2)
-        receiver(
-            f"Charge : {charge_sensor.get_value()}, Temperature : {heat_sensor.get_value()}")
+        charge_stream.append(charge_sensor.get_value())
+        heat_stream.append(heat_sensor.get_value())
+
+    print_on_console(charge_stream, heat_stream)
 
 
-def print_on_console(params):
-    print(params)
+def print_on_console(charge_stream, heat_stream):
+    print("Charge")
+    for i in charge_stream:
+        print(i, end=",")
+    print()
+    print("Heat")
+    for i in heat_stream:
+        print(i, end=",")
 
 
 if __name__ == "__main__":
