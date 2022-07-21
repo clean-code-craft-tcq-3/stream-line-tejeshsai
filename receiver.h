c2f89c162c8b410ac7a00a65eabcace72fb0fc77
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-07-21 13:22:45 +0530

    Update receiver.h

--- receiver.h
+++ receiver.h
@@ -2,3 +2,22 @@
 #include <vector>
 #include<string>
 #include <assert.h>
+
+struct TemperatureStatistics
+{
+   minimumReadings;
+   maximumReadings;
+   simpleMovingAvg;
+}
+struct StateOfChargeStatistics
+{
+   minimumReadings;
+   maximumReadings;
+   simpleMovingAvg;
+}
+
+struct BatteryStatistics
+{     
+  TemperatureStatistics tempreatureStats;
+  StateOfChargeStatistics stateOfChargeStats;
+}
