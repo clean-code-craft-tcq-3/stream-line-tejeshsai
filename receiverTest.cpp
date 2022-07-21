e487a00094c13315e2bf66c44cd59481f27bc2eb
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-07-21 19:20:24 +0530

    Update receiverTest.cpp

--- receiverTest.cpp
+++ receiverTest.cpp
@@ -82,7 +82,7 @@ int main()
   
   TestCheckMinimumStateofCharge();
   TestCheckMaximumStateofCharge();
-  TestStaetOfChargeSimpleMovingAverage();
+ // TestStaetOfChargeSimpleMovingAverage();
   
   TestBatteryStatistics();
   
