b1be22bf1e2b7c11b81ed7ef10f1ffaa7e614e7e
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-07-21 15:56:58 +0530

    Update receiverTest.cpp

--- receiverTest.cpp
+++ receiverTest.cpp
@@ -4,23 +4,26 @@
 void TestCheckMinimumTemperature()
 {
   float temperatureMin;
-  temperatureMin = getMinimumTemperature(temperatureReadings);
-  assert(temperatureMin == 0); 
+  std::vector<float> temperatureReadings = {10,2,30,20,5};
+  temperatureMin = getMinimumTemperatureReadings(temperatureReadings);
+  assert(temperatureMin == 2); 
 }
 
 //Test Maximum Temperature
 void TestCheckMaximumTemperature()
 {
   float temperatureMax;
-  temperatureMax = getMaximumTemperature(temperatureReadings);
-  assert(TemperatureMax == 0); 
+  std::vector<float> temperatureReadings = {10,2,30,20,5};
+  temperatureMax = getMaximumTemperatureReadings(temperatureReadings);
+  assert(TemperatureMax == 30); 
 }
 
 //Test Minimum state of charge
 void TestCheckMinimumStateofCharge()
 {
   float stateOfChargeMin;
-  stateOfChargeMin = getMinimumStateofCharge(stateofChargeReadings);
+  std::vector<float> stateofChargeReadings = {60,68,70,74,80};
+  stateOfChargeMin = getMinimumStateOfChargeReadings(stateofChargeReadings);
   assert(stateOfChargeMin == 60); 
 }
 
@@ -28,7 +31,8 @@ void TestCheckMinimumStateofCharge()
 void TestCheckMaximumStateofCharge()
 {
   float stateOfChargeMax;
-  stateOfChargeMax = getMaximumStateofCharge(stateofChargeReadings);
+  std::vector<float> stateofChargeReadings = {60,68,70,74,80};
+  stateOfChargeMax = getMaximumStateOfChargeReadings(stateofChargeReadings);
   assert(stateOfChargeMax == 80); 
 }
 
@@ -36,32 +40,36 @@ void TestCheckMaximumStateofCharge()
 void TestStaetOfChargeSimpleMovingAverage()
 {
   float stateOfChargeSMA; // SMA - Simple Moving Avergae
+  std::vector<float> stateofChargeReadings = {60,68,70,74,80};
   stateOfChargeSMA = getSimpleMovingAverage(stateofChargeReadings);
-  assert(stateOfChargeSMA == 50); 
+  assert(stateOfChargeSMA == 70.4); 
 }
 
 //Test SimpleMovingAvergae of Temperature
 void TestTemperatureSimpleMovingAverage()
 {
   float TemperatureSMA; // SMA - Simple Moving Avergae
+  std::vector<float> temperatureReadings = {10,2,30,20,5};
   TemperatureSMA = getSimpleMovingAverage(temperatureReadings);
-  assert(TemperatureSMA == 50); 
+  assert(TemperatureSMA == 13.4); 
 }
 
 //Test Battery Statistics
 void TestBatteryStatistics()
 {
   BatteryStatistics batteryStats;
+  std::vector<float> temperatureReadings = {10,2,30,20,5};
+  std::vector<float> stateofChargeReadings = {60,68,70,74,80};
   batteryStats = computeStatistics(temperatureReadings,stateofChargeReadings);
 
   // Test Temperature Stats
-  assert(batteryStats.tempereatureStats.minimumReadings == 0);
-  assert(batteryStats.tempereatureStats.maximumReadings == 60);
-  assert(batteryStats.tempereatureStats.simpleMovingAvg == 50);
+  assert(batteryStats.tempereatureStats.minimumReadings == 2);
+  assert(batteryStats.tempereatureStats.maximumReadings == 30);
+  assert(batteryStats.tempereatureStats.simpleMovingAvg == 13.4);
   //Test StateOfChargeStats
   assert(batteryStats.stateOfChargeStats.minimumReadings == 60);
   assert(batteryStats.stateOfChargeStats.maximumReadings == 80);
-  assert(batteryStats.stateOfChargeStats.simpleMovingAvg == 50);
+  assert(batteryStats.stateOfChargeStats.simpleMovingAvg == 70.4);
 }
 
 void TestDisplayBatteryStatistics()
