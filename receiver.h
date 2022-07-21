fa7fd0875cbab578d959e286dec0aefb0978ed76
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-07-21 13:30:05 +0530

    Update receiver.h

--- receiver.h
+++ receiver.h
@@ -18,6 +18,15 @@ struct StateOfChargeStatistics
 
 struct BatteryStatistics
 {     
-  TemperatureStatistics tempreatureStats;
+  TemperatureStatistics tempereatureStats;
   StateOfChargeStatistics stateOfChargeStats;
 }
+
+std::vector<float> getTemperatureReadings(std::vector<float> temperatureReadings);
+std::vector<float> getStateOfChargeReadings(std::vector<float> stateOfChargeReadings);
+float getMinimumTemperatureReadings(std::vector<float> temperatureReadings);
+float getMaximumTemperatureReadings(std::vector<float> temperatureReadings);
+float getMinimumStateOfChargeReadings(std::vector<float> stateOfChargeReadings);
+float getMaximumStateOfChargeReadings(std::vector<float> stateOfChargeReadings);
+float getSimpleMovingAverage(std::vector<float> readings);
+BatteryStatistics computeStatistics(std::vector<float> readings1 , std::vector<float> readings2);
